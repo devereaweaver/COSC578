5fa1ef47296746cf19ce3bb7f24870cfe61c67e3
devereaweaver <deveredev@proton.me>
2024-04-28 23:19:30 -0400

    completed question i

--- assignment_database/question_i.sql
+++ assignment_database/question_i.sql
@@ -1,1 +1,16 @@
--- TODO
+-- find the names and addresses of employees who work on at least one
+-- project located in houston but whose department has no location in Htown. 
+
+select fname, minit, lname, address
+from employee e
+join works_on w on e.ssn = w.essn
+join project p on w.pno = p.pnumber
+where p.plocation = 'Houston'
+  and e.dno not in (
+    select d.dnumber
+    from department d, dept_locations l
+    where d.dnumber in (
+		select l.dnumber
+			where dlocation != 'Houston'
+    )
+  );
