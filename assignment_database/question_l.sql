301dd0633b0dccda317885dd0dd81461cbab684e
devereaweaver <deveredev@proton.me>
2024-04-28 23:22:07 -0400

    completed question l

--- assignment_database/question_l.sql
+++ assignment_database/question_l.sql
@@ -1,1 +1,7 @@
-
+-- Find the names of employees who are working on the project 
+-- that is not controlled by the emloyeees own department.
+select fname, minit, lname 
+from employee, project, works_on 
+where ssn = essn 
+and pnumber = pno 
+and dnum != dno;
