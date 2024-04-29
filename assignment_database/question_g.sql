4f7fdbab4b87fc6c79187087741e4f0120c50e50
devereaweaver <deveredev@proton.me>
2024-04-28 22:55:05 -0400

    complete question g

--- assignment_database/question_g.sql
+++ assignment_database/question_g.sql
@@ -1,13 +1,7 @@
--- TODO: Complete this
-
 -- For each deparment, retreive the department names, and total 
 -- salary of employees working in that department
 
--- aggregate the salary's by dno first 
-select dno, sum(salary)
-from employee e, department d
-group by dno;
-
-select department_name
-from department
-where 
+select dname, sum(salary) 
+from department d, employee e
+where e.dno = d.dnumber
+group by d.dnumber;
