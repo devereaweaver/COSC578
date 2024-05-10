4f433822ac0193d4162a1735cb7ef206f3bd9eca
daw <deveredev@gmail.com>
2024-05-10 17:31:15 -0400

    updated populating database

---
+++ project_database/populate_data.sql
@@ -0,0 +1,15 @@
+insert into  attorney (fname, minit, lname, specialization, attorney_phone, address, office_number)
+values 
+    ('Adam', 'A', 'Adams', 'Corporate', '1111111111', '123 Baltimore St, Baltimore, MD', '111'),
+    ('Bobby', 'B', 'Bobs', 'Patent', '2222222222', '321 Baltimore St, Baltimore, MD', '112'),
+    ('Chris', 'C', 'Christopher', 'Accident', '3333333333', '213 Baltimore St, Baltimore, MD', '113'),
+    ('Davis', 'D', 'Davis', 'Corporate', '4444444444', '312 Baltimore St, Baltimore, MD', '114'),
+	('Erick', 'E', 'Ericks', 'Patent', '5555555555', '123 St. Paul St, Baltimore, MD', '115'),
+    ('Faith', 'F', 'Feathers', 'Contract', '6666666666', '321 St. Paul St, Baltimore, MD', '116'), 
+    ('Greg', 'G', 'Gregory', 'Small Business', '7777777777', '456 St. Paul St, Baltimore, MD', '117'), 
+    ('Heather', 'H', 'Hurts', 'Trademark', '8888888888', '789 Orleans St, Baltimore, MD', '118'), 
+    ('Ines', 'I', 'Iverson', 'Securities', '9999999999', '987 Orleans St, Baltimore, MD', '119'),
+    ('Jackie', 'J', 'Jackson', 'Contract', '1234567890', '555 Baltimore St, Baltimore, MD', '120');
+
+
+select * from attorney;
